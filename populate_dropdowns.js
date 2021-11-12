291eeed9b803b208137e9f867df6589032690fa3
Caleb McIrvin <calebmcirvin111@gmail.com>
2021-11-12 13:07:27 -0500

    States now update when a country is selected

--- populate_dropdowns.js
+++ populate_dropdowns.js
@@ -18,3 +18,24 @@ function populateCountries() {
         sel.appendChild(opt);
     }
 }
+
+function populateStates() {
+    let countries = httpGet("https://xc-countries-api.herokuapp.com/api/countries/");
+    var country = document.getElementById('countries');
+    var name = country.options[country.selectedIndex].value;
+    var code;
+    for (var i = 0; i < countries.length; i++) {
+        if (countries[i]["name"] === name) {
+            code = countries[i]["code"];
+            break;
+        }
+    }
+    let states = httpGet("https://xc-countries-api.herokuapp.com/api/countries/" + code + "/states/");
+    var sel = document.getElementById('states');
+    for(var i = 0; i < states.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = states[i]["name"];
+        opt.value = states[i]["name"];
+        sel.appendChild(opt);
+    }
+}
