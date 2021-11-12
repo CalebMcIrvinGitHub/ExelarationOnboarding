ce2bba24a8b57f8751284f3bd4e0d74a41f1e9da
Caleb McIrvin <calebmcirvin111@gmail.com>
2021-11-12 12:23:03 -0500

    Added some JS functionality

--- populate_dropdowns.js
+++ populate_dropdowns.js
@@ -1,3 +1,14 @@
-function test(){
-    alert('Test');
+const RAPIDAPI_API_URL = 'https://arjunkomath-jaas-json-as-a-service-v1.p.rapidapi.com/';
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function populateCountries() {
+    let countries = httpGet("https://xc-countries-api.herokuapp.com/api/countries/");
+    console.log(countries);
 }
