329a9d088503e77cd623fb5365ba9d3d123152fb
Caleb McIrvin <calebmcirvin111@gmail.com>
2021-11-12 12:45:20 -0500

    Filled countries dropdown with countries

--- populate_dropdowns.js
+++ populate_dropdowns.js
@@ -2,13 +2,19 @@ const RAPIDAPI_API_URL = 'https://arjunkomath-jaas-json-as-a-service-v1.p.rapida
 
 function httpGet(theUrl)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send();
+    return JSON.parse(xmlHttp.response);
 }
 
 function populateCountries() {
     let countries = httpGet("https://xc-countries-api.herokuapp.com/api/countries/");
-    console.log(countries);
+    var sel = document.getElementById('countries');
+    for(var i = 0; i < countries.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = countries[i]["name"];
+        opt.value = countries[i]["name"];
+        sel.appendChild(opt);
+    }
 }
